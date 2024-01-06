d633498fd62b9ebfd1134d92d2d817e31860473c
juanr.pacha <juan.ramon.pacha@upc.edu>
2024-01-06 12:55:40 +0100

    An example of 1D arrays has been added

--- src/main.cc
+++ src/main.cc
@@ -6,17 +6,33 @@ int main()
   const size_t COLS = 5;
   const size_t PREC = 2;
 
-  // Declare permutation vector and set all its components to 0
-  std::vector<int> p(ROWS, 0);
+  // Vectors
 
-  // Declare matrices a and aa and set all their components to 0.0
+  std::vector<double> v(ROWS, 0.0e00);
+
+  std::cout << "- Initial Vector V: " << std::endl;
+  print_vector<double>(v, PREC);
+  std::cout << std::endl;
+
+  for (auto & component : v)component += 3.0e00;
+
+  std::cout << "- New vector V: " << std::endl;
+  print_vector<double>(v, PREC);
+  std::cout << std::endl;
+
+  // Martrices
+  //
+  // Declare permutation vector and set all its components to 0
+  std::vector<int> perm(ROWS, 0);
+
+  // Declare matrices a and a_copy and set all their components to 0.0
   std::vector<std::vector<double>>
       a(ROWS, std::vector<double>(COLS, 0.0e0)),
-      aa(ROWS, std::vector<double>(COLS, 0.0e0));
+      a_copy(ROWS, std::vector<double>(COLS, 0.0e0));
 
   // Set the permutation vector p = [1,2,3,...,ROWS]
-  for (size_t i = 0; i < p.size(); ++i)
-    p[i] = i;
+  for (size_t i = 0; i < perm.size(); ++i)
+    perm[i] = i;
 
   // Set the values for matrix a to: a[i][j] = i+1, i = 0,...,ROWS-1,
   // j = 0,...,COLS-1
@@ -24,10 +40,10 @@ int main()
     for (size_t j = 0; j < a[i].size(); ++j)
       a[i][j] = static_cast<double>(i + 1);
 
-  aa = a; // Copy matrix a to matrix aa
+  a_copy = a; // Copy matrix a to matrix a_copy
 
   std::cout << "- Initial permutation vector p: " << std::endl;
-  print_vector<int>(p, PREC);
+  print_vector<int>(perm, PREC);
   std::cout << std::endl;
 
   std::cout << "- Initial matrix A: " << std::endl;
@@ -35,42 +51,42 @@ int main()
   std::cout << std::endl;
 
   std::cout << "- Copy of matrix A: " << std::endl;
-  print_matrix<double>(aa, PREC);
+  print_matrix<double>(a_copy, PREC);
   std::cout << std::endl;
 
   // Switch the 1st & 4th components of permutation vector p
-  std::swap(p[0], p[3]);
+  std::swap(perm[0], perm[3]);
 
   // Switch the 1st & 4th rows of matrix a
   a[0].swap(a[3]);
 
   // Print the new permutation vector
   std::cout << "- New permutation vector (1st & 4th components switched): " << std::endl;
-  print_vector<int>(p, PREC);
+  print_vector<int>(perm, PREC);
   std::cout << std::endl;
 
   std::cout << "- New matrix A (1st & 4th rows switched): " << std::endl;
   print_matrix<double>(a, PREC);
   std::cout << std::endl;
 
-  p = {3, 2, 0, 1}; // New permutation vector
+  perm = {3, 2, 0, 1}; // New permutation vector
   std::cout << "- New permutation vector: " << std::endl;
-  print_vector<int>(p, PREC);
+  print_vector<int>(perm, PREC);
   std::cout << std::endl;
 
   // Permutem les files de la matriu a
-  for (size_t i = 0; i < p.size(); i++)
-    a[i] = aa[p[i]];
+  for (size_t i = 0; i < perm.size(); i++)
+    a[i] = a_copy[perm[i]];
 
   std::cout << "- Matrix A with the files switched: " << std::endl;
   print_matrix<double>(a, PREC);
   std::cout << std::endl;
 
   // Deallocae permutation vector p
-  std::vector<int>().swap(p);
-  std::cout << "- Deallocate pointer p " << std::endl
-            << "  Size: " << p.size() << std::endl
-            << "  Capacity: " << p.capacity() << std::endl
+  std::vector<int>().swap(perm);
+  std::cout << "- Deallocate pointer perm " << std::endl
+            << "  Size: " << perm.size() << std::endl
+            << "  Capacity: " << perm.capacity() << std::endl
             << std::endl;
 
   // Deallocate matrix a
@@ -90,9 +106,9 @@ int main()
             << "  Column of pointers capacity: " << a.capacity() << std::endl
             << std::endl;
 
-  // Deallocate matrix aa
-  std::cout << "- Deallocate matrix aa " << std::endl;
-  for (auto &element : aa)
+  // Deallocate matrix a_copy
+  std::cout << "- Deallocate matrix a_copy " << std::endl;
+  for (auto &element : a_copy)
   {
     std::vector<double>::iterator iter;
     std::cout << "  ";
@@ -102,9 +118,9 @@ int main()
     std::cout << "Row size: " << element.size() << ", "
               << "Row capacity: " << element.capacity() << std::endl;
   }
-  std::vector<std::vector<double>>().swap(aa);
-  std::cout << "  Column of pointers size: " << aa.size() << std::endl
-            << "  Column of pointers capacity: " << aa.capacity() << std::endl;
+  std::vector<std::vector<double>>().swap(a_copy);
+  std::cout << "  Column of pointers size: " << a_copy.size() << std::endl
+            << "  Column of pointers capacity: " << a_copy.capacity() << std::endl;
 
   return EXIT_SUCCESS;
 }
